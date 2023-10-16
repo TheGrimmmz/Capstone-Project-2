726aefa267309eacee5dda4f59795b72ef233aea
Nick Mitchell <119639651+TheGrimmmz@users.noreply.github.com>
2023-10-16 15:35:07 -0500

    added flowchart

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
+/**
+ * Handles an event and creates a payment intent.
+ *
+ * @param {Object} e - the event object containing the request body
+ * @return {Object} - the response object with the payment intent or error message
+ */
 const handler = async (e) => {
     try{
        const { amount } = JSON.parse(e.body)
